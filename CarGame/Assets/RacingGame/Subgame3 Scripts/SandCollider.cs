66b618a405b47a915fb794c805b685247139287d
nlin3696 <38114559+nlin3696@users.noreply.github.com>
2019-11-08 06:23:24 -0500

    Added sand slowing feature

---
+++ CarGame/Assets/RacingGame/Subgame3 Scripts/SandCollider.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SandCollider : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            other.gameObject.GetComponent<Rigidbody>().drag = 2.5f;
+            Debug.Log("In");
+        }
+    }
+
+    public void OnTriggerExit(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            other.gameObject.GetComponent<Rigidbody>().drag = 0f;
+            Debug.Log("Out");
+        }
+    }
+
+    /*
+    public void OnTriggerEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "")
+        {
+            this.GetComponent<Rigidbody>().drag = 1.5f;
+            Debug.Log(collision.gameObject.name);
+        }
+    }
+
+    public void OnTriggerExit(Collision collision)
+    {
+        if (collision.gameObject.tag == "SandArea")
+        {
+            this.GetComponent<Rigidbody>().drag = 0;
+            Debug.Log(collision.gameObject.name);
+        }
+    }*/
+}
