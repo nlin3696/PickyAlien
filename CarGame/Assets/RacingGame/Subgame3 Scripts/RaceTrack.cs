3649c21ed9be3d357398b9a95702e4dae21ea2ee
nlin3696 <38114559+nlin3696@users.noreply.github.com>
2019-11-15 01:46:12 -0500

    Minor tweaks to track

---
+++ CarGame/Assets/RacingGame/Subgame3 Scripts/RaceTrack.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RaceTrack : MonoBehaviour
+{
+
+    public void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("track");
+        if (other.tag == "Player")
+        {
+            Debug.Log("on track");
+        }
+    }
+
+}
